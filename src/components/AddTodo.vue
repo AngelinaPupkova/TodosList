60dfc8b887718110acc982fd442728ba830f1d60
Angelina Pupkova <angelinapupkova@MacBook-Air-Angelina.local>
2021-02-08 22:17:37 +0100

    Initial commit

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,40 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <label>
+      <input v-model="title" type="text">
+    </label>
+    <button type= "submit" >Create</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: ''
+    }
+  },
+  methods: {
+    onSubmit() {
+      if (this.title.trim()) {
+        const newTodo = {
+          id: Date.now(),
+          title: this.title,
+          completed : false
+        }
+        this.$emit('add-todo', newTodo)
+        this.title = ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  form {
+    display: flex;
+  }
+  input{
+    width: 500px;
+  }
+</style>
